20cdd9c0f4dbc91740419c23e33c3e8a5ff3b081
mikey-roberts <mikey_roberts@outlook.com>
2021-08-11 09:30:26 +0100

    feat: display google map from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,20 @@
 import { useEffect, useState } from "react";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+require('dotenv').config();
+
+
+
+function MyMap() {
+  return (
+  <GoogleMap 
+  defaultZoom={6}
+  defaultCenter={{ lat: 55.378052, lng: -3.435973 }} 
+  />
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(MyMap));
+
 
 function App() {
   const [data, setData] = useState(null);
@@ -10,13 +26,33 @@ function App() {
       ).catch(error => console.log(error));
   }, []);
 
+
   return (
     <div className="App">
-      <header className="App-header">
         <p>{!data ? "Loading..." : data}</p>
-      </header>
+
+        <div className="mapps-container" 
+        style={{
+        width: '80vw', 
+        height: '80vh',
+        borderStyle: 'solid'}}>
+
+        <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_MAPS_API_KEY
+        }`} 
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+
+
+        />
+        </div>
     </div>
+
   );
 }
+
+
 
 export default App;
