39477bf42f47fa55fe843b7ad5e068f4d7331cf4
Fabio <alessf13@gmail.com>
2021-08-11 16:48:23 +0300

    fix: server communication from front to back and google maps api

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
 import "./App.css";
-import { Hikes } from './components/Hikes'
+import { Hikes } from "./components/Hikes";
 import { withScriptjs, withGoogleMap } from "react-google-maps";
-import MyMap from './components/maps/MyMap'
+import MyMap from "./components/maps/MyMap";
 const WrappedMap = withScriptjs(withGoogleMap(MyMap));
-require('dotenv').config();
-
+require("dotenv").config();
 
 function App() {
-
   return (
-    <div className="maps-container" >
-      <WrappedMap 
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.REACT_APP_MAPS_API_KEY
-        }`} 
+    <div
+      className="maps-container"
+      style={{ width: "80vw", height: "80vh", borderStyle: "solid" }}
+    >
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_MAPS_API_KEY}`}
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
       />
-    <Hikes />
-
-  </div>
+      <Hikes />
+    </div>
   );
 }
-
-
 
 export default App;
 
