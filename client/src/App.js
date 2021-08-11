b26ff00b91552c68c77bd9d9058e49a5a4a8a834
Frank Mckenna <fmckenna56330@gmail.com>
2021-08-11 13:54:00 +0100

    fix: conflict errors due to merges

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,32 @@
+import React from "react";
+import "./App.css";
+import { Hikes } from './components/Hikes'
 import { withScriptjs, withGoogleMap } from "react-google-maps";
 import MyMap from './components/maps/MyMap'
 const WrappedMap = withScriptjs(withGoogleMap(MyMap));
 require('dotenv').config();
-import React from "react";
-import "./App.css";
-import { Hikes } from './components/Hikes'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="maps-container" 
-        style={{
-        width: '80vw', 
-        height: '80vh',
-        borderStyle: 'solid'}}>
 
-        <WrappedMap 
+  return (
+    <div className="maps-container" >
+      <WrappedMap 
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
           process.env.REACT_APP_MAPS_API_KEY
         }`} 
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
-        />
-        </div>
+      />
+    <Hikes />
 
-  return (
-    <div className="App">
-      <Hikes />
-    </div>
-
+  </div>
   );
 }
 
 
 
 export default App;
+
+// style={{ width: '80vw', height: '80vh', borderStyle: 'solid'}}
