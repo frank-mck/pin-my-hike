bdf1f51f3f2648fd57c467649fd4f3353dcb9b6d
Cynthia Fu <yinnycfu@gmail.com>
2021-08-16 16:35:55 +0100

    chore: add api route, connect to the db

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use("/api/v1/restaurants", restaurants)
+app.use("/api/v1/hikes", hikes)
 
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
 
