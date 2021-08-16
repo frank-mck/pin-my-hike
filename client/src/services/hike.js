95e585875ff96dd364f416cb261c9e595079891e
Cynthia Fu <yinnycfu@gmail.com>
2021-08-16 21:50:32 +0100

    chore: add get(id) function to fe HikeDataService

--- client/src/services/hike.js
+++ client/src/services/hike.js
@@ -1,21 +1,22 @@
 import http from "../http-common";
 
+// includes functions that make API calls and return the information from API calls
 class HikeDataService {
-    getAll() {
-        return http.get();
-      }
-    
-      // get(id) {
-      //   return http.get(`/id/${id}`);
-      // }
-    
-      // find(query, by = "title", page = 0) {
-      //   return http.get(`?${by}=${query}&page=${page}`);
-      // } 
-    
-      createHike(data) {
-        return http.post("/new", data);
-      }
+  getAll() {
+    return http.get();
+  }
+
+  createHike(data) {
+    return http.post("/new", data);
+  }
+  
+  get(id) {
+    return http.get(`/id/${id}`);
+  }
+
+  // find(query, by = "title", page = 0) {
+  //   return http.get(`?${by}=${query}&page=${page}`);
+  // } 
 }
     
 export default new HikeDataService();
