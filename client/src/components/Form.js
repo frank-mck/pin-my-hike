63c62920bd53a224ef2612c13ef3d4fd313d7b04
Frank Mckenna <fmckenna56330@gmail.com>
2021-08-17 21:43:14 +0100

    style: styles to form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -29,33 +29,40 @@ export const Form = ({ setPins, pins, location, onAdd, setMarkers, setSelected }
   }
 
   return (
-    <form className="form" onSubmit={onSubmit} >
-     <button className='close-btn' onClick={close}></button>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity={`${process.env.FONT_AWESOME_CDN}`} crossorigin="anonymous" />
+    <body>
+      <form className="form" onSubmit={onSubmit} >
+        <header>
+          <img alt ='hiking logo' src ='https://image.flaticon.com/icons/png/512/3373/3373903.png'></img>
+          <h2>Add your pin</h2>
+          <button className='close-btn' onClick={close}></button>
+        </header>
+        
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity={`${process.env.FONT_AWESOME_CDN}`} crossorigin="anonymous" />
 
-      <input  className="form-title" type ='text' name='title' placeholder='Enter title...' value={title} onChange={(e) => setTitle(e.target.value)}></input>
-      <textarea  className="form-text-field" name ='description' id="description" type="text" rows="5"
-        placeholder="Enter a description of your hike" value={description} onChange={(e) => setDescription(e.target.value)}>
-      </textarea>
+        <input  className="form-title" type ='text' name='title' placeholder='Enter title...' value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        <textarea  className="form-text-field" name ='description' id="description" type="text" rows="5"
+          placeholder="Enter a description of your hike" value={description} onChange={(e) => setDescription(e.target.value)}>
+        </textarea>
 
-      <br />
+        <br />
 
-      <div className="form-footer">
+        <div className="form-footer">
 
-        <div className="form-logo-container">
-          <label htmlFor="photo" className="form-footer-photo-label">
-            Upload - 
-            <div><i class="fas fa-camera fa-1x form-footer-logo"></i></div>
-          </label>
-          <input type="file" id="photo" style={{display: "none"}} />
+          <div className="form-logo-container">
+            <label htmlFor="photo" className="form-footer-photo-label">
+              Upload - 
+              <div><i class="fas fa-camera fa-1x form-footer-logo"></i></div>
+            </label>
+            <input type="file" id="photo" style={{display: "none"}} />
+          </div>
+
+          <div className="form-submit-container">
+            <button type="submit" className="btn form-submit">Submit</button>
+          </div>
+
         </div>
-
-        <div className="form-submit-container">
-          <button type="submit" className="btn form-submit">Submit</button>
-        </div>
-
-      </div>
-    </form>
+      </form>
+    </body>
   )
 }
 
