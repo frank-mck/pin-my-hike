8024fdd45c344cbdd9759c03e751e9da89f1ae94
Frank Mckenna <fmckenna56330@gmail.com>
2021-08-17 13:53:24 +0100

    chore: deleted hikes.js and refactored

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -9,8 +9,6 @@ export const Form = ({ setPins, pins, location, onAdd, setMarkers }) => {
 
   const addPin = async (pin) => {
    HikeDataService.createHike(pin)
-   
-
     setPins([...pins, pin])
   }
 
@@ -27,15 +25,15 @@ export const Form = ({ setPins, pins, location, onAdd, setMarkers }) => {
 
   return (
     <form className="form" onSubmit={onSubmit} >
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity={`${process.env.FONT_AWESOME_CDN}`} crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity={`${process.env.FONT_AWESOME_CDN}`} crossorigin="anonymous" />
       <input required className="form-title" type ='text' name='title' placeholder='Enter title...' value={title} onChange={(e) => setTitle(e.target.value)}></input>
       <textarea required className="form-text-field" name ='description' id="description" type="text" cols="40" rows="5"
         placeholder="Enter a description of your hike" value={description} onChange={(e) => setDescription(e.target.value)}>
       </textarea>
       <br />
       <label htmlFor="photo" className="form-photo-label">
-          <div><i class="fas fa-camera fa-3x form-upload-image"></i></div>
-        </label>
+        <div><i class="fas fa-camera fa-3x form-upload-image"></i></div>
+      </label>
       <button type="submit" className="btn form-submit">Submit</button>
     </form>
   )
