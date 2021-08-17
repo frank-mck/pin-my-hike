ecde4336e74b4c4a045e7b6c46eeb7bf1b3d974a
mikey-roberts <mikey_roberts@outlook.com>
2021-08-17 15:42:24 +0100

    chore: gitignore node module clean-up

--- server.js
+++ server.js
@@ -2,6 +2,28 @@ import express from "express"
 import cors from "cors"
 import hikes from "./api/hikes.route.js"
 
+
+
+
+// const multer = require('multer')
+
+// const fileStorageEngine = multer.diskStorage({
+//   destination: (req, res, cb) => {
+//     cb(null, './images')
+//   },
+//   filename: (req, res, cb) => {
+//     cb(null, Date.now() + '--' + file.originalname)
+//   }
+// })
+
+// const upload = multer({storage : fileStorageEngine})
+
+// app.post("/single", upload.single('image'),(res,
+// res) => {
+//   console.log(req.file)
+//   res.send('Single file upload successful')
+// })
+
 const app = express()
 
 app.use(cors())
