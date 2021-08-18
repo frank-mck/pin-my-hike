37b1a1ba4a9a3ba0fbe95eaf9c7945a7ef6461de
Fabio <alessf13@gmail.com>
2021-08-18 18:54:55 +0300

    style: change color to search bar

--- client/src/components/maps/MyMap.js
+++ client/src/components/maps/MyMap.js
@@ -161,6 +161,7 @@ export function MyMap() {
           }}
         >
           <ComboboxInput
+            className="search-input"
             value={value}
             onChange={(e) => {
               setValue(e.target.value);
@@ -168,11 +169,26 @@ export function MyMap() {
             disabled={!ready}
             placeholder="Enter an address"
           />
-          <ComboboxPopover>
-            {status === "OK" &&
-              data.map(({ id, description }) => (
-                <ComboboxOption key={id} value={description} />
-              ))}
+          <ComboboxPopover portal={false}>
+            {data.length > 0 ? (
+              <ComboboxList className="search-results">
+                {status === "OK" &&
+                  data.map(({ id, description }) => (
+                    <ComboboxOption key={id} value={description} />
+                  ))}
+              </ComboboxList>
+            ) : (
+              <p
+                style={{
+                  margin: 0,
+                  color: "#454545",
+                  padding: "0.25rem 1rem 0.75rem 1rem",
+                  fontStyle: "italic",
+                }}
+              >
+                No results
+              </p>
+            )}
           </ComboboxPopover>
         </Combobox>
       </div>
