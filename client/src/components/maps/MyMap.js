c1f8b78bce17b39e483c0b05897838f6b31b5937
Fabio <alessf13@gmail.com>
2021-08-19 17:46:45 +0300

    Feat: Search component refactored out of MyMap

--- client/src/components/maps/MyMap.js
+++ client/src/components/maps/MyMap.js
@@ -1,25 +1,18 @@
 import React from "react";
 import "../../styles/App.css";
-import { AddPin } from '../AddPin.js'
-import { Form } from '../Form.js'
-import { Button } from './Button.js'
-import { Confirmation } from '../Confirmation.js'
-import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api"
+import { AddPin } from "../AddPin.js";
+import { Form } from "../Form.js";
+import { Button } from "./Button.js";
+import { Confirmation } from "../Confirmation.js";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import mapStyle from "../../styles/mapStyle.js";
 import HikeDataService from "../../services/hike.js";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-  ComboboxOptionText,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
+import Search from "./Search";
 
 const libraries = ["places"];
 
@@ -30,8 +23,12 @@ const mapContainerStyle = {
 
 export const MyMap = () => {
   const [markers, setMarkers] = React.useState([]);
-  const [latitude, setLatitude] = React.useState(markers.length === 0 ? parseFloat(55.378052) : markers.map(n =>n.lat));
-  const [longitude, setLongitude] = React.useState(markers.length === 0 ? parseFloat(-3.435973) : markers.map(n =>n.lng));
+  const [latitude, setLatitude] = React.useState(
+    markers.length === 0 ? parseFloat(55.378052) : markers.map((n) => n.lat)
+  );
+  const [longitude, setLongitude] = React.useState(
+    markers.length === 0 ? parseFloat(-3.435973) : markers.map((n) => n.lng)
+  );
   const [zoom, setZoom] = React.useState(8);
   const [pins, setPins] = React.useState([]);
   const [selected, setSelected] = React.useState(null);
@@ -116,9 +113,9 @@ export const MyMap = () => {
   }, []);
 
   const fetchPins = () => {
-     HikeDataService.getAll()
-      .then(response => {
-        setPins(response.data.hikes);  
+    HikeDataService.getAll()
+      .then((response) => {
+        setPins(response.data.hikes);
       })
       .catch((e) => {
         console.log(e);
@@ -128,27 +125,27 @@ export const MyMap = () => {
   // Sets Marker position based off the click event on the map.
 
   const onClickNewMarker = (event) => {
-      if (dropPin === true) {
-        setMarkers(() => [
-          {
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng(),
-            time: new Date(),
-          }
-        ])
+    if (dropPin === true) {
+      setMarkers(() => [
+        {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng(),
+          time: new Date(),
+        },
+      ]);
     } else {
       return null;
     }
-  }
+  };
 
   // Toggle enables user to drop a pin on the map.
 
   const toggle = () => {
     setTimeout(() => {
-      setDropPin(() => setDropPin(!dropPin))
-    }, 10)
-    if (dropPin) setMarkers([])
-  }
+      setDropPin(() => setDropPin(!dropPin));
+    }, 10);
+    if (dropPin) setMarkers([]);
+  };
 
   if (loadError) return "Error handling maps";
   if (!isLoaded) return "Loading Maps";
@@ -160,184 +157,143 @@ export const MyMap = () => {
     setSelected(null);
   };
 
-  function Search() {
-    const {
-      ready,
-      value,
-      suggestions: { status, data },
-      setValue,
-      clearSuggestions,
-    } = usePlacesAutocomplete({
-      requestOptions: {
-        location: { lat: () => latitude, lng: () => longitude },
-        radius: 200 * 1000,
-      },
-    });
-
-    return (
-      <div className="search">
-        <script src="https://kit.fontawesome.com/1fc4ea1c6a.js" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity={`${process.env.FONT_AWESOME_CDN}`} crossorigin="anonymous" />
-        <Combobox
-          className="search-box"
-          onSelect={async (address) => {
-            setValue(address, false);
-            clearSuggestions();
-            try {
-              const results = await getGeocode({ address });
-              const { lat, lng } = await getLatLng(results[0]);
-              console.log({ lat, lng });
-              setLatitude(lat);
-              setLongitude(lng);
-            } catch (error) {
-              console.log(error);
-            }
-            console.log(address);
-          }}
-        >
-          <button class="btn-search"><i class="fas fa-search"></i></button>
-
-          <ComboboxInput
-            className="input-search"
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-            disabled={!ready}
-            placeholder="Enter an address"
-          />
-          <ComboboxPopover portal={false}>
-            {data.length > 0 ? (
-              <ComboboxList className="search-results">
-                {status === "OK" &&
-                  data.map(({ id, description }) => (
-                    <ComboboxOption key={id} value={description} />
-                  ))}
-              </ComboboxList>
-            ) : (
-              <p
-                style={{
-                  margin: 0,
-                  color: "#454545",
-                  padding: "0.25rem 1rem 0.75rem 1rem",
-                  fontStyle: "italic",
-                }}
-              >
-                No results
-              </p>
-            )}
-          </ComboboxPopover>
-        </Combobox>
-      </div>
-    );
-  }
-
   // Initializes Google map with preset values.
-
 
   return (
     <GoogleMap
-    mapContainerStyle={mapContainerStyle}
-    zoom={zoom}
-    center={{ lat: latitude, lng: longitude }}
-    options={{
-      styles: mapStyle,
-      disableDefaultUI: true,
-      zoomControl: true,
-      minZoom: 4,
-      maxZoom: 28,
-     }}
+      mapContainerStyle={mapContainerStyle}
+      zoom={zoom}
+      center={{ lat: latitude, lng: longitude }}
+      options={{
+        styles: mapStyle,
+        disableDefaultUI: true,
+        zoomControl: true,
+        minZoom: 4,
+        maxZoom: 28,
+      }}
       onClick={onClickNewMarker}
     >
+      {pins.map((hike) => (
+        // Maps hikes, setting their position based off lat and lng stored in DB.
 
-    {pins.map((hike) => (
+        <Marker
+          key={hike._id}
+          position={{ lat: hike.lat, lng: hike.lng }}
+          icon={{
+            url: "https://img.icons8.com/color/48/000000/camping-tent.png",
+            scaledSize: new window.google.maps.Size(45, 45),
+            anchor: new window.google.maps.Point(20, 20),
+          }}
+          onClick={() => {
+            setSelectedHike(hike);
+          }}
+        />
+      ))}
 
-    // Maps hikes, setting their position based off lat and lng stored in DB.  
+      <div>
+        <Search
+          lat={latitude}
+          lng={longitude}
+          positionSetter={positionSetter}
+          zoomIn={zoomIn}
+        />
+      </div>
 
-      <Marker
-        key={hike._id}
-        position={{ lat: hike.lat, lng: hike.lng }}
-        icon={{
-          url: "https://img.icons8.com/color/48/000000/camping-tent.png",
-          scaledSize: new window.google.maps.Size(45, 45),
-          anchor: new window.google.maps.Point(20, 20),
-        }}
-        onClick={() => {
-          setSelectedHike(hike);
-        }}
-      />
-    ))}
-
-    <div>
-      <Search />
-    </div>
-
-        <div>
+      <div>
         {selectedHike ? (
+          // InfoWindow to display each hikes attributes.
 
-        // InfoWindow to display each hikes attributes. 
-        
-        <InfoWindow
-          className="info-window"
-          position={{ "lat": parseFloat(selectedHike.lat), "lng": parseFloat(selectedHike.lng) }}
-          onCloseClick={() => {setSelectedHike(null)}}>
-          <div className ='pin-description'>
-            <h2>Title - { selectedHike.title } </h2>
-            <p>Description - { selectedHike.description } </p>
-            
-            {selectedHike.image ?
-              (
-              <div><img src={ selectedHike.image }
-                alt=""
-                height="260px"
-                width="250px"
-            ></img></div>
-               ) 
-               :
-              (
-              <div><img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/happy-campers-live-here-unknown.jpg"
-                alt=""
-                height="260px"
-                width="250px"
-            ></img></div>
-              )
-            }
-          </div>
-        </InfoWindow>) : null }
+          <InfoWindow
+            className="info-window"
+            position={{
+              lat: parseFloat(selectedHike.lat),
+              lng: parseFloat(selectedHike.lng),
+            }}
+            onCloseClick={() => {
+              setSelectedHike(null);
+            }}
+          >
+            <div className="pin-description">
+              <h2>Title - {selectedHike.title} </h2>
+              <p>Description - {selectedHike.description} </p>
+
+              {selectedHike.image ? (
+                <div>
+                  <img
+                    src={selectedHike.image}
+                    alt=""
+                    height="260px"
+                    width="250px"
+                  ></img>
+                </div>
+              ) : (
+                <div>
+                  <img
+                    src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/happy-campers-live-here-unknown.jpg"
+                    alt=""
+                    height="260px"
+                    width="250px"
+                  ></img>
+                </div>
+              )}
+            </div>
+          </InfoWindow>
+        ) : null}
+      </div>
+
+      {markers.map((marker) => {
+        return (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: "https://i.ibb.co/74nnLy7/pin-my-hike-trial.png",
+              scaledSize: new window.google.maps.Size(75, 75),
+              anchor: new window.google.maps.Point(35, 60),
+            }}
+          />
+        );
+      })}
+
+      {current.map((marker) => {
+        return (
+          <Marker
+            key={marker.time.toISOString()}
+            className="button-container"
+            position={{ lat: marker.lat, lng: marker.lng }}
+            zoom={zoom}
+            icon={{
+              url: "https://image.flaticon.com/icons/png/512/4985/4985836.png",
+              scaledSize: new window.google.maps.Size(45, 45),
+              anchor: new window.google.maps.Point(20, 45),
+            }}
+          />
+        );
+      })}
+
+      {selected ? (
+        <div>
+          <Form
+            setSelected={setSelected}
+            pins={pins}
+            setPins={setPins}
+            onAdd={addNewPin}
+            setMarkers={setMarkers}
+            location={{ lat: selected.lat, lng: selected.lng }}
+          />
         </div>
+      ) : null}
 
-            {markers.map(marker => {
-              return <Marker key={marker.time.toISOString()}
-              position={{lat: marker.lat, lng: marker.lng}} 
-              icon={{
-                url: 'https://i.ibb.co/74nnLy7/pin-my-hike-trial.png',
-                scaledSize: new window.google.maps.Size(75,75),
-                anchor: new window.google.maps.Point(35,60),
-              }}
-              
-            />
-            })}
+      <Button getPosition={getPosition} />
 
-           {current.map(marker => {
-              return <Marker key={marker.time.toISOString()}
-              className='button-container'
-              position={{lat: marker.lat, lng: marker.lng}} 
-              zoom={zoom}
-              icon={{
-                url: 'https://image.flaticon.com/icons/png/512/4985/4985836.png',
-                scaledSize: new window.google.maps.Size(45,45),
-                anchor: new window.google.maps.Point(20,45),              
-              }}
-            />
-            })}
-            
-              {selected ? ( <div><Form setSelected={setSelected} pins={pins} setPins={setPins} onAdd={addNewPin}
-               setMarkers={setMarkers} location={{lat: selected.lat, lng: selected.lng}} /></div> ) : null  }
-
-               <Button getPosition={getPosition} />
-               
-               <AddPin toggle={toggle} />
-                { markers.length > 0 && <Confirmation toggle={toggle} setMarkers={setMarkers} confirm={() => setSelected(markers[0])}  />}
-            </GoogleMap>
-  )
-}
+      <AddPin toggle={toggle} />
+      {markers.length > 0 && (
+        <Confirmation
+          toggle={toggle}
+          setMarkers={setMarkers}
+          confirm={() => setSelected(markers[0])}
+        />
+      )}
+    </GoogleMap>
+  );
+};
